45b7eec2c7f0aaa9ebce6cb17128c2cb90296d07
saikiran2693 <79732848+saikiran2693@users.noreply.github.com>
2021-09-02 01:48:00 -0400

    Add files via upload

---
+++ privacy-policy.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Crime Report's</title>
+
+    <?php
+    include_once 'linkFiles.php';
+    ?>
+</head>
+<body>
+
+<!-- HEADER-->
+<?php include "publicheader.php"; ?>
+<!-- //HEADER-->
+
+<div class="container my-5">
+    <h2 class="text-center text-underline mb-3">Privacy Policy</h2>
+
+    <div class="row">
+        <div class="col-lg-6">
+            <img src="img/privacy-policy.jpg" style="width: 100%;border-radius: 10px" alt="Privacy Policy">
+        </div>
+        <div class="col-lg-6">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+                into electronic typesetting, remaining essentially unchanged.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+                into electronic typesetting, remaining essentially unchanged.</p>
+        </div>
+    </div>
+</div>
+
+<!-- Footer -->
+<div class="fixed-bottom">
+    <?php include "footer.php"; ?>
+</div>
+<!-- Footer -->
+
+</body>
+</html>
