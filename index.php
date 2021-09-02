5a8b2178f5bae393aefd131d9c3c919e2d2e99e6
saikiran2693 <79732848+saikiran2693@users.noreply.github.com>
2021-09-02 01:47:19 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+
+    <?php
+    include_once 'linkFiles.php';
+    ?>
+</head>
+<body>
+
+<!-- HEADER-->
+<?php include "publicheader.php"; ?>
+<!-- //HEADER-->
+
+<div class="container my-5 py-5 alert-primary border">
+    <h2 class="text-center text-underline mb-3">Welcome To Home Page</h2>
+
+</div>
+
+<!-- Footer -->
+<div class="fixed-bottom">
+    <?php include "footer.php"; ?>
+</div>
+<!-- Footer -->
+
+</body>
+</html>
