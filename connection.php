029e944dec954c3c7087295652f47b5c875cbc89
tania1994924 <74213357+tania1994924@users.noreply.github.com>
2021-09-02 19:14:54 -0400

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,5 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', null, 'assg_crime');
+if(!$conn){
+    die('connection failed..!!');
+}
---
+++ publicheader.php
@@ -0,0 +1,36 @@
+<!-- HEADER-->
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    <a class="navbar-brand" href="index.php">
+        <img src="img/logo/logo.jpeg" width="180" height="90" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!--        <ul class="navbar-nav mx-auto">-->
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="text-white nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="text-white nav-link" href="view-all-reports.php">Crime Report's</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="text-white nav-link" href="privacy-policy.php">Privacy Policy</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="text-white nav-link" href="admin-login.php">Admin Login</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="text-white nav-link" href="user-signup.php">User Signup</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<!-- //HEADER-->
