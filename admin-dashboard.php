dff2db6a737ae1a51eff1712b7938a4cf51ef60c
Chintan-1997 <79673306+Chintan-1997@users.noreply.github.com>
2021-09-09 09:04:30 -0400

    Add files via upload

---
+++ admin-dashboard.php
@@ -0,0 +1,35 @@
+<?php
+@session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Dashboard</title>
+
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+          integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+
+<?php include_once "adminheader.php" ?>
+
+<div class="container my-5 ">
+    <div class="jumbotron text-center">
+        <h2>Welcome Admin <i class="text-success far fa-smile"></i></h2>
+    </div>
+</div>
+
+<div class="fixed-bottom">
+    <?php include_once "footer.php" ?>
+</div>
+
+</body>
+</html>
