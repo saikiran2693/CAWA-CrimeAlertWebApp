12d6065d55c40a1ac2d5c8d4ecce7c9504333a4f
tania1994924 <74213357+tania1994924@users.noreply.github.com>
2021-09-15 18:52:04 -0400

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer class="page-footer font-small bg-dark">
     <div class="text-white footer-copyright text-center py-3">© 2021 Copyright:
-        <a class="text-white" href="javacript:void(0);"> cawa</a>
+        <a class="text-white" href="javacript:void(0);"> CAWA</a>
     </div>
 </footer>
 <!-- Footer -->
