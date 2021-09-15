015a3a060233263d784a0cd19023900bc05f4593
saikiran2693 <79732848+saikiran2693@users.noreply.github.com>
2021-09-15 00:10:09 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Home Page</title>
@@ -7,23 +8,28 @@
     <?php
     include_once 'linkFiles.php';
     ?>
+
 </head>
+
 <body>
 
-<!-- HEADER-->
-<?php include "publicheader.php"; ?>
-<!-- //HEADER-->
+    <!-- HEADER-->
+    <?php include "publicheader.php"; ?>
+    <!-- //HEADER-->
 
-<div class="container my-5 py-5 alert-primary border">
-    <h2 class="text-center text-underline mb-3">Welcome To Home Page</h2>
-
-</div>
-
-<!-- Footer -->
-<div class="fixed-bottom">
-    <?php include "footer.php"; ?>
-</div>
-<!-- Footer -->
+    <div class="container my-5 py-5 alert-primary border">
+        <h2 class="text-center text-underline mb-3">Welcome To CAWA - Crime Alert Web App</h2>
+        <div class="jumbotron text-center">
+            <img src="img/logo/logo.jpeg" width="500" height="230" alt="">
+        </div>
+    </div>
+    
+    <!-- Footer -->
+    <div class="fixed-bottom">
+        <?php include "footer.php"; ?>
+    </div>
+    <!-- Footer -->
 
 </body>
+
 </html>
